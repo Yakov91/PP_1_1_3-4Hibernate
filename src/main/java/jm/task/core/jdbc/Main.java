7ccf1312b74c818efbce7570b27448530abff230
Ykov91 <yakov.litvinenko.91@mail.ru>
2024-01-18 22:00:09 +0900

    commit main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,13 +8,13 @@ import jm.task.core.jdbc.util.Util;
 public class Main {
     public static void main(String[] args) {
         UserServiceImpl userService = new UserServiceImpl();
-        userService.createUsersTable();
+//        userService.createUsersTable();
         userService.saveUser("Ivan", "Ivanov", (byte) 22);
         userService.saveUser("Piter", "Petrov", (byte) 26);
         userService.saveUser("Bob", "Steven", (byte) 33);
         userService.saveUser("Jack", "Vorobey", (byte) 43);
         System.out.println(userService.getAllUsers());
-        userService.cleanUsersTable();
-     //   userService.dropUsersTable();
+//        userService.cleanUsersTable();
+//        userService.dropUsersTable();
     }
 }
